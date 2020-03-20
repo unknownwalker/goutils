c8a8922582b8f04417ec89d8d9c222da9fb1718e
jiangzhaohua <jiangzhaohua2001@126.com>
2020-03-20 14:08:38 +0800

    blockCopy

---
+++ blockCopy/blockCopy.go
@@ -0,0 +1,20 @@
+package blockCopy
+
+import "errors"
+
+func blockCopy(src []byte, srcOffset int, dst []byte, dstOffset, count int) (bool, error) {
+	srcLen := len(src)
+	if srcOffset > srcLen || count > srcLen || srcOffset+count > srcLen {
+		return false, errors.New("源缓冲区 索引超出范围")
+	}
+	dstLen := len(dst)
+	if dstOffset > dstLen || count > dstLen || dstOffset+count > dstLen {
+		return false, errors.New("目标缓冲区 索引超出范围")
+	}
+	index := 0
+	for i := srcOffset; i < srcOffset+count; i++ {
+		dst[dstOffset+index] = src[srcOffset+index]
+		index++
+	}
+	return true, nil
+}
