253fbde8a86e0d27424410ac553a3c232dc492b1
jiangzhaohua <jiangzhaohua2001@126.com>
2020-04-22 18:19:59 +0800

    M

--- bcrypt/BCP.go
+++ bcrypt/BCP.go
@@ -117,7 +117,7 @@ func Dncrypt(rawData string, key []byte) (string, error) {
 	return string(dnData), nil
 }
 
-func md5V(str string) string {
+func Md5V(str string) string {
 	h := md5.New()
 	h.Write([]byte(str))
 	return hex.EncodeToString(h.Sum(nil))
